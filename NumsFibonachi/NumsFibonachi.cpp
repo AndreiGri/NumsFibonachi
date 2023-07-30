4202440808f2119ebeb79dcb9246038561e7e138
Gridin_Andrei <andreigridin918@gmail.com>
2023-07-30 19:08:20 +0500

    Добавьте файлы проекта.

---
+++ NumsFibonachi/NumsFibonachi.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int n, s1 = 0, s2 = 0, r = 0;
+    cout << "Введите количество чисел Фибоначчи: ";
+    cin >> n;
+    //n = n - 2;
+    while (n) {
+        if (s1 == 0) {
+            s1++;
+            cout << s1 << " ";
+        }
+        s2 += s1;
+        cout << s2 << " ";
+        n--;
+        r = s1;
+        s1 = s2;
+        s2 = r;
+    }
+    system("pause > nul");
+    return 0;
+}
